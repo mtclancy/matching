e755d50295aa4e40336e8747ae63633d2c2f35a6
Michael Clancy <michaelclancy@michaels-air.mhsnt.muscatine.k12.ia.us>
2018-05-31 10:26:00 -0500

    match functionality

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 resetButton = document.getElementById('reset');
 
 //creates random array, and places icons on the board
+window.onload = resetBoard;
+
 function setPieces() {
     let gamePieces = ["fa fa-anchor", "fa fa-anchor", "fa fa-automobile", "fa fa-automobile", "fa fa-bank", "fa fa-bank", "fa fa-bath", "fa fa-bath", "fa fa-bed", "fa fa-bed", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bug", "fa fa-bug", "fa fa-child", "fa fa-child"];
 
@@ -15,10 +17,12 @@ function setPieces() {
         let space = document.getElementById("card"+i);
         let setIcon = document.createElement('i');
         space.appendChild(setIcon).setAttribute('class', currentRound[i]);
+        setIcon.setAttribute('id','icon'+i);
     }   
 }
 
 //removes existing icons when board is reset
+
 function deleteIcons() {
     for (i=0; i<16; i++) {
         let cardNum = document.getElementById('card'+i);
@@ -29,6 +33,7 @@ function deleteIcons() {
 }
 
 //combines two functions that allow the board to be reset when button is clicked
+
 function resetBoard() {
     deleteIcons();
     setPieces();
@@ -42,9 +47,34 @@ resetButton.addEventListener('click', resetBoard);
 function pickedItem() {
     for (i=0; i < 16; i++) {
         let userChoice = document.getElementById('card'+i);
+        let userIcon = document.getElementById('icon'+i);
         let changeColor = function() {
             userChoice.className = 'picked';
+            userIcon.style.visibility = 'visible';
+            firstClick();
         } 
         userChoice.addEventListener('click',changeColor);
     }
+}
+
+//if the class and styles match, trigger event.
+function firstClick() {
+    let firstPick = document.getElementsByClassName('picked')[0];
+    let firstIcon = firstPick.firstChild.getAttribute('class');
+    let secondPick = document.getElementsByClassName('picked')[1];
+    let secondIcon = secondPick.firstChild.getAttribute('class');
+    if (firstIcon === secondIcon) {
+        firstPick.style.backgroundColor = 'green';
+        secondPick.style.backgroundColor = 'green';
+        firstPick.classList.remove('picked');
+        secondPick.classList.remove('picked');} else {
+            firstPick.style.backgroundColor = 'red';
+            secondPick.style.backgroundColor = 'red';
+        let firstIcon = firstPick.firstChild;
+        let secondIcon = secondPick.firstChild;
+            firstIcon.style.visibility = 'hidden';
+            secondIcon.style.visibility = 'hidden';
+            firstPick.classList.remove('picked');
+            secondPick.classList.remove('picked'); 
+        };
 }
